beb2291195567b48283122c370c7ffe234aad897
Lukas Niemeier <lukas.niemeier@koeln.de>
2019-08-03 11:18:54 +0200

    Players Key Calibrator

---
+++ GMTKGJ2019/Assets/_Application/Scripts/Calibrator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GMTKGJ2019
+{
+    public class Calibrator : MonoBehaviour
+    {
+        private static readonly double TotalCalibrationTime = TimeSpan.FromSeconds(5).TotalSeconds;
+
+        [SerializeField] private Text playersText = null;
+
+        [SerializeField] private ProgressBar timeLeft = null;
+
+        private bool hasCalibrationStarted = false;
+        private double calibrationTimeLeft = TotalCalibrationTime;
+
+        private readonly List<KeyCode> keys = new List<KeyCode>();
+
+        public List<KeyCode> GetPlayersKeys()
+        {
+            return new List<KeyCode>(keys);
+        }
+
+        private void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                hasCalibrationStarted = true;
+            }
+
+            if (!hasCalibrationStarted)
+            {
+                return;
+            }
+
+            calibrationTimeLeft -= Time.deltaTime;
+
+            if (calibrationTimeLeft > 0f)
+            {
+                foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+                {
+                    if (Input.GetKeyDown(key))
+                    {
+                        keys.Add(key);
+                    }
+
+                    if (Input.GetKeyUp(key))
+                    {
+                        keys.Remove(key);
+                    }
+                }
+
+                timeLeft.SetProgress((float)((100f / TotalCalibrationTime) * calibrationTimeLeft));
+                //timeLeftText.text = TimeSpan.FromSeconds(calibrationTimeLeft).ToString();
+                playersText.text = String.Join(" ", keys);
+            }
+            else
+            {
+                timeLeft.SetProgress(0f);
+            }
+        }
+    }
+}
---
+++ GMTKGJ2019/Assets/_Application/Scripts/ProgressBar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace GMTKGJ2019
+{
+    public class ProgressBar : MonoBehaviour
+    {
+
+        [SerializeField] private UnityEngine.UI.Image fillImage = null;
+
+        private float fullWidth;
+
+        private float currentWidth;
+
+        public void SetProgress(float percentage)
+        {
+            currentWidth = Math.Max(0f, (fullWidth / 100) * percentage);
+        }
+
+        private void Start()
+        {
+            fullWidth = fillImage.rectTransform.rect.width;
+            currentWidth = fullWidth;
+        }
+
+        private void Update()
+        {
+            var delta = fillImage.rectTransform.sizeDelta;
+            fillImage.rectTransform.sizeDelta = new Vector2(currentWidth, delta.y);
+        }
+    }
+}
