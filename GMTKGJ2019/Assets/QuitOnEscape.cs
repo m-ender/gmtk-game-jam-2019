fe5eeda8561671fe2c8f8c66ae270640b03da9e4
Lukas Niemeier <lukas.niemeier@koeln.de>
2019-08-04 12:27:20 +0200

    Quit on ESC

---
+++ GMTKGJ2019/Assets/QuitOnEscape.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GMTKGJ2019
+{
+    public class QuitOnEscape : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
